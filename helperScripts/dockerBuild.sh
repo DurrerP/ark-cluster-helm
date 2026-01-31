31b4e7b3c1f3307e1617d1648ec2c7edb67bec04
DurrerP <>
2026-01-31 19:08:29 +0100

    minmal helper file adjustments

--- helperScripts/dockerBuild.sh
+++ helperScripts/dockerBuild.sh
@@ -5,6 +5,9 @@ docker buildx build . -t ghcr.io/durrerp/ark-cluster-helm:latest --no-cache
 
 docker buildx build . -t ghcr.io/durrerp/ark-cluster-helm:latest -t ghcr.io/durrerp/ark-cluster-helm:v0.0.1
 
+docker push ghcr.io/durrerp/ark-cluster-helm:latest
+docker push ghcr.io/durrerp/ark-cluster-helm:v0.0.1
+
 
 docker run -p 7777:7777 -p 7778:7778 -p 27015:27015 -it ghcr.io/durrerp/ark-cluster-helm:latest
 
